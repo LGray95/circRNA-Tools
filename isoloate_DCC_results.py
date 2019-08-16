3f66a601015a1d7e9d032f0c71263fa3352fd2ee
LGray95 <lachlan.gray95@gmail.com>
2019-08-16 13:28:18 +1000

    -Turned into a function

--- isoloate_DCC_results.py
+++ isoloate_DCC_results.py
@@ -1,24 +1,38 @@
-in_DCC = open('/Users/lachlan/Documents/Masters_Research_2019/Data/DCC/Epilepsy/SUDEPmRNA_103187/CircRNACount.txt').read().splitlines()
+def isolate_DCC_results(infile, outdir):
 
-for k in range(3,51):
-    out_list = []
-    for i in in_DCC:
-        col = i.split('\t')
-        chr = col[0]
-        start = col[1]
-        end = col[2]
-        sample = col[k]
-        out_list.append(chr+":"+start+"-"+end+'\t'+sample)
+# Where:
+# Infile is the DCC output file
+# Outdir is the directory where you want to store the results
+
+    # Counts the total columns of the file to parse to range()
+    with open(infile) as file:
+        line = file.readline()
+    total_columns = int(len(line.split()))
+
+    # Open file
+    in_DCC = open(infile).read().splitlines()
+    # Read one columns at a time
+    for k in range(3, total_columns):
+        out_list = []
+        for i in in_DCC:
+            col = i.split('\t')
+            chr = col[0]
+            start = col[1]
+            end = col[2]
+            sample = col[k]
+            out_list.append(chr+":"+start+"-"+end+'\t'+sample)
 
 
-    filtered_out_list = []
-    sample_ID =(out_list[0].split('\t')[1])
-    print(sample_ID)
-    for i in out_list[1:-1]:
-        if int(i.split('\t')[1]) >= 2:
-            filtered_out_list.append(i)
+        filtered_out_list = []
+        sample_ID =(out_list[0].split('\t')[1])
+        print(sample_ID)
+        # Filter each circRNA for at least two BSJ reads
+        for i in out_list[1:-1]:
+            if int(i.split('\t')[1]) >= 2:
+                filtered_out_list.append(i)
 
-    outfile = open('/Users/lachlan/Documents/Masters_Research_2019/Data/DCC/Epilepsy/SUDEPmRNA_103187/'+sample_ID+'.txt', 'w')
-    for i in filtered_out_list:
-        outfile.write(str(i) + '\n')
-    outfile.close()
+        # Write to file
+        outfile = open(outdir+sample_ID+'.txt', 'w')
+        for i in filtered_out_list:
+            outfile.write(str(i) + '\n')
+        outfile.close()
